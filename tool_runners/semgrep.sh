082914a46641358826bd789f1f8795b0c8b20006
Aleksandr Misonizhnik <misonijnik@gmail.com>
2024-05-20 00:31:38 +0200

    fix: Update `semgrep.sh`

--- tool_runners/semgrep.sh
+++ tool_runners/semgrep.sh
@@ -2,15 +2,15 @@
 
 # Check for install/updates at https://github.com/returntocorp/semgrep
 
-docker pull returntocorp/semgrep > /dev/null
+docker pull semgrep/semgrep > /dev/null
 
 entry_point=$1
 cd $entry_point
 
-semgrep_version=$(docker run --rm returntocorp/semgrep semgrep --version)
+semgrep_version=$(docker run --rm semgrep/semgrep semgrep --version)
 result_filename="Semgrep-v$semgrep_version.sarif"
 
-docker run --rm -v "${PWD}:/src" returntocorp/semgrep semgrep --config auto -q --sarif -o "$result_filename" . > /dev/null
+docker run --rm -v "${PWD}:/src" semgrep/semgrep semgrep --config auto -q --sarif -o "$result_filename" . > /dev/null
 docker run --rm -v "${PWD}:/src" ubuntu sh -c "chown $(id -u $USER):$(id -g $USER) -R /src" > /dev/null
 
 result_file="$entry_point/$result_filename"
