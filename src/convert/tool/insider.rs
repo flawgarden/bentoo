356da8805b835eda55a9c55fdf1030663a7da23f
Aleksandr Misonizhnik <misonijnik@gmail.com>
2024-10-02 16:22:23 +0300

    chore: Rewrite `insider` converter using the Deserialize attribite (#37)

--- src/convert/tool/insider.rs
+++ src/convert/tool/insider.rs
@@ -1,80 +1,86 @@
+use serde::Deserialize;
+use serde_json::Value;
 use std::{fs, path::Path};
 
-use serde_json::Value;
 use serde_sarif::sarif::{
     ArtifactLocationBuilder, LocationBuilder, MessageBuilder, PhysicalLocationBuilder,
     RegionBuilder, ResultBuilder, RunBuilder, Sarif, SarifBuilder, ToolBuilder,
     ToolComponentBuilder,
 };
 
-fn from_json(json: &Value) -> Sarif {
+#[derive(Debug, Deserialize)]
+struct InsiderReport {
+    pub vulnerabilities: Vec<Vulnerability>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Vulnerability {
+    pub cwe: String,
+    pub line: i64,
+    pub column: i64,
+    pub class_message: String,
+    pub description: String,
+}
+
+fn from_json(json: Value) -> Sarif {
     let mut results = vec![];
-
-    if let Value::Array(vulns) = &json["vulnerabilities"] {
-        let mut result_builder = ResultBuilder::default();
-        for vul in vulns {
-            if let Value::String(cwe) = &vul["cwe"] {
-                result_builder.rule_id(cwe);
-            }
-            if let Value::Number(line) = &vul["line"] {
-                if let Value::Number(column) = &vul["column"] {
-                    if let Value::String(message) = &vul["classMessage"] {
-                        let line = line.as_i64().unwrap();
-                        let column = column.as_i64().unwrap();
-                        let region = RegionBuilder::default()
-                            .start_line(line)
-                            .end_line(line)
-                            .start_column(column)
-                            .end_column(column)
-                            .build()
-                            .unwrap();
-                        let message = message.as_str().split(' ').next().unwrap();
-                        let artifact = ArtifactLocationBuilder::default()
-                            .uri(message)
-                            .build()
-                            .unwrap();
-                        let location = PhysicalLocationBuilder::default()
-                            .artifact_location(artifact)
-                            .region(region)
-                            .build()
-                            .unwrap();
-                        let location = LocationBuilder::default()
-                            .physical_location(location)
-                            .build()
-                            .unwrap();
-                        result_builder.locations(vec![location]);
-                    }
-                }
-            }
-            if let Value::String(description) = &vul["description"] {
-                let sarif_message = MessageBuilder::default().text(description).build().unwrap();
-                result_builder.message(sarif_message);
-            }
-            results.push(result_builder.build().unwrap());
-        }
-        let tool = ToolBuilder::default()
-            .driver(
-                ToolComponentBuilder::default()
-                    .name("insider")
-                    .build()
-                    .unwrap(),
-            )
+    let insider_report: InsiderReport = serde_json::from_value(json).unwrap();
+    let mut result_builder = ResultBuilder::default();
+    for vul in insider_report.vulnerabilities {
+        result_builder.rule_id(vul.cwe);
+        let line = vul.line;
+        let column = vul.column;
+        let region = RegionBuilder::default()
+            .start_line(line)
+            .end_line(line)
+            .start_column(column)
+            .end_column(column)
             .build()
             .unwrap();
-        let run = RunBuilder::default()
-            .tool(tool)
-            .results(results)
+        let message = vul.class_message.as_str().split(' ').next().unwrap();
+        let artifact = ArtifactLocationBuilder::default()
+            .uri(message)
             .build()
             .unwrap();
-
-        SarifBuilder::default()
-            .version("2.1.0")
-            .runs(vec![run])
+        let location = PhysicalLocationBuilder::default()
+            .artifact_location(artifact)
+            .region(region)
             .build()
-            .unwrap()
-    } else {
-        panic!("Parsing failed.");
+            .unwrap();
+        let location = LocationBuilder::default()
+            .physical_location(location)
+            .build()
+            .unwrap();
+        result_builder.locations(vec![location]);
+        let sarif_message = MessageBuilder::default()
+            .text(vul.description)
+            .build()
+            .unwrap();
+        result_builder.message(sarif_message);
+        results.push(result_builder.build().unwrap());
     }
+
+    let tool = ToolBuilder::default()
+        .driver(
+            ToolComponentBuilder::default()
+                .name("insider")
+                .build()
+                .unwrap(),
+        )
+        .build()
+        .unwrap();
+    let run = RunBuilder::default()
+        .tool(tool)
+        .results(results)
+        .build()
+        .unwrap();
+
+    SarifBuilder::default()
+        .version("2.1.0")
+        .runs(vec![run])
+        .build()
+        .unwrap()
 }
 
 pub fn from_file(path: &Path) -> Sarif {
@@ -84,5 +90,5 @@ pub fn from_file(path: &Path) -> Sarif {
 
 pub fn from_string(string: &str) -> Sarif {
     let json: Value = serde_json::from_str(string).unwrap();
-    from_json(&json)
+    from_json(json)
 }
